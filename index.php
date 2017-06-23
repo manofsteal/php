5ad7e9e311ee849f0e6aeb98d01e9bf78ae8b85f
Van Nam <realman.document@gmail.com>
2017-06-23 11:44:29 +0700

    Modify code

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
-<<html>
+<html>
 <head>
 	<title></title>
+	<meta charset="UTF-8">
 </head>
 <body>
-	<a href="1.html">hello</a>
+	<h1>Công thức làm bánh</h1>
+	<ul>
+		<li><a href="1.html">1</a></li>
+		<li><a href="2.html">2</a></li>
+		<li><a href="3.html">3</a></li>
+		<li><a href="4.html">4</a></li>
+		<li><a href="5.html">5</a></li>
+	</ul>
+	
 </body>
 </html>
