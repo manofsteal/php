e6f19d3ebf6756df260acfe21ed997ef509463ab
Van Nam <realman.document@gmail.com>
2017-06-26 16:14:43 +0700

    Modiy index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/2.html">
-	      <img src="image/2.png" alt="Forest" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/2.PNG" alt="Forest" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -30,7 +30,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/3.html">
-	      <img src="image/3.png" alt="Northern Lights" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/3.PNG" alt="Northern Lights" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -39,7 +39,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/4.html">
-	      <img src="image/4.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/4.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -48,7 +48,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/5.html">
-	      <img src="image/5.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/5.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -57,7 +57,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/6.html">
-	      <img src="image/6.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/6.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -66,7 +66,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/7.html">
-	      <img src="image/7.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/7.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -75,7 +75,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/8.html">
-	      <img src="image/8.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/8.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -84,7 +84,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/9.html">
-	      <img src="image/9.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/9.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -93,7 +93,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/10.html">
-	      <img src="image/10.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/10.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -102,7 +102,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/11.html">
-	      <img src="image/11.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/11.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -111,7 +111,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/12.html">
-	      <img src="image/12.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/12.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -120,7 +120,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/13.html">
-	      <img src="image/13.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/13.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -129,7 +129,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/14.html">
-	      <img src="image/14.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/14.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -138,7 +138,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/15.html">
-	      <img src="image/15.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/15.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -147,7 +147,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/16.html">
-	      <img src="image/16.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/16.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -156,7 +156,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/17.html">
-	      <img src="image/17.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/17.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -165,7 +165,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/18.html">
-	      <img src="image/18.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/18.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -174,7 +174,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/19.html">
-	      <img src="image/19.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/19.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -183,7 +183,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/20.html">
-	      <img src="image/20.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/20.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -192,7 +192,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/21.html">
-	      <img src="image/21.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/21.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -201,7 +201,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/22.html">
-	      <img src="image/22.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/22.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -210,7 +210,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/23.html">
-	      <img src="image/23.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/23.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -219,7 +219,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/24.html">
-	      <img src="image/24.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/24.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -228,7 +228,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/25.html">
-	      <img src="image/25.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/25.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -237,7 +237,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/26.html">
-	      <img src="image/26.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/26.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -246,7 +246,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/27.html">
-	      <img src="image/27.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/27.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -255,7 +255,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/28.html">
-	      <img src="image/28.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/28.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -264,7 +264,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/29.html">
-	      <img src="image/29.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/29.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -274,7 +274,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/30.html">
-	      <img src="image/30.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/30.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -284,7 +284,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/31.html">
-	      <img src="image/31.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/31.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -294,7 +294,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/32.html">
-	      <img src="image/32.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/32.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
@@ -304,7 +304,7 @@
 	<div class="responsive">
 	  <div class="gallery">
 	    <a target="_blank" href="fb/33.html">
-	      <img src="image/33.png" alt="Mountains" width="600" height="400">
+	      <img src="https://raw.githubusercontent.com/manofsteal/php/master/image/33.PNG" alt="Mountains" width="600" height="400">
 	    </a>
 	    <div class="desc">Add a description of the image here</div>
 	  </div>
