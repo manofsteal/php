baab5262d3925bb265f8d0adff8351e0a8ede199
Van Nam <realman.document@gmail.com>
2017-06-26 10:57:17 +0700

    Modify code

--- index.php
+++ index.php
@@ -2,16 +2,314 @@
 <head>
 	<title></title>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
+
 </head>
 <body>
-	<h1>Công thức làm bánh</h1>
-	<ul>
-		<li><a href="1.html">1</a></li>
-		<li><a href="2.html">2</a></li>
-		<li><a href="3.html">3</a></li>
-		<li><a href="4.html">4</a></li>
-		<li><a href="5.html">5</a></li>
-	</ul>
 	
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/1.html">
+	      <img src="image/1.png" alt="Trolltunga Norway" width="300" height="200">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/2.html">
+	      <img src="image/2.png" alt="Forest" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/3.html">
+	      <img src="image/3.png" alt="Northern Lights" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/4.html">
+	      <img src="image/4.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/5.html">
+	      <img src="image/5.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/6.html">
+	      <img src="image/6.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/7.html">
+	      <img src="image/7.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/8.html">
+	      <img src="image/8.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/9.html">
+	      <img src="image/9.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/10.html">
+	      <img src="image/10.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/11.html">
+	      <img src="image/11.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/12.html">
+	      <img src="image/12.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/13.html">
+	      <img src="image/13.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/14.html">
+	      <img src="image/14.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/15.html">
+	      <img src="image/15.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/16.html">
+	      <img src="image/16.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/17.html">
+	      <img src="image/17.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/18.html">
+	      <img src="image/18.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/19.html">
+	      <img src="image/19.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/20.html">
+	      <img src="image/20.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/21.html">
+	      <img src="image/21.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/22.html">
+	      <img src="image/22.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/23.html">
+	      <img src="image/23.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/24.html">
+	      <img src="image/24.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/25.html">
+	      <img src="image/25.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/26.html">
+	      <img src="image/26.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/27.html">
+	      <img src="image/27.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/28.html">
+	      <img src="image/28.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/29.html">
+	      <img src="image/29.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/30.html">
+	      <img src="image/30.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/31.html">
+	      <img src="image/31.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/32.html">
+	      <img src="image/32.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+
+	<div class="responsive">
+	  <div class="gallery">
+	    <a target="_blank" href="fb/33.html">
+	      <img src="image/33.png" alt="Mountains" width="600" height="400">
+	    </a>
+	    <div class="desc">Add a description of the image here</div>
+	  </div>
+	</div>
+
+
 </body>
 </html>
